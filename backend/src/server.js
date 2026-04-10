a33b510395176d8e153b085cb2a700f8c518fa67
Anshika Srivastava <anshika.io143@gmail.com>
2026-04-10 14:14:56 +0530

    created backend folder structure and basic express server

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config() ;
+const mongoose = require("mongoose") ;
+
+const express = require("express") ;
+
+
+const app = express() ;
+
+
+
+
+
+//db connection 
+
+const MONGO_URI = process.env.MONGO_URI ;
+
+const dbConnect = mongoose.connect(MONGO_URI)
+.then(() => {
+    console.log(" connected to mongoDB ");
+})
+.catch((error)=>{
+    console.error("Error connecting to mongodb",error) ;
+})
+
+//server connection 
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`server is listening on port ${PORT}`)
+})
